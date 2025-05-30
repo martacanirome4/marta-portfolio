248fc5a7837b2b85686b3b4c31f9959950b5a72a
MartaCanino <martacaninoromero@gmail.com>
2025-05-30 15:55:13 +0200

    fix unsued-vars error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 import React, { useState, useEffect } from 'react';
 import { Github, Linkedin, Mail, ExternalLink, Award, Telescope, Brain, Database, Cpu, ArrowRight, Download } from 'lucide-react';
 
